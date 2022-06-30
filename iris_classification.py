5034ad7a5adeabbe700bdd272b64607148720783
Pohyee02 <108342910+Pohyee02@users.noreply.github.com>
2022-06-30 21:46:12 +0800

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -56,9 +56,7 @@ else:
     st.write('')
 
 st.subheader('Tell us which is your favourite?')
-species = st.radio(
-     "What's your favorite flower?",
-     ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'))
+species = st.radio(('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'))
 
 if species == 'Iris-setosa':
      st.write('You like Iris-setosa.')
