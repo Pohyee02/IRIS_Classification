3c0b38ac3ecd1bf0efad01e5825ae7f9171657d6
Pohyee02 <108342910+Pohyee02@users.noreply.github.com>
2022-06-30 20:55:48 +0800

    Update iris_classification.py

--- iris_classification.py
+++ iris_classification.py
@@ -38,7 +38,8 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
+df = pd.DataFrame({'label' : ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']})
+st.table(df)
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
